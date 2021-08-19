383f87379921eac2eb6ccb792141e147f24e9981
sayaka <syknxgw@gmail.com>
2021-08-19 21:35:43 +0900

    finished route part

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,8 +1,6 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
-import {v4 as uuid} from 'uuid';
-
 export default class ActivityStore {
     activityRegistry =  new Map<string, Activity>();
     selectedActivity: Activity | undefined = undefined;
@@ -74,7 +72,6 @@ export default class ActivityStore {
 
     createActivity = async (activity: Activity) => {
         this.loading = true;
-        activity.id = uuid();
 
         try {
             await agent.Activities.create(activity);
